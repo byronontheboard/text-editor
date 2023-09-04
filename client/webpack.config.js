d2896127611d4f7b6a13b340aea150d23b64c166
byronallan <127366720+byronallan@users.noreply.github.com>
2023-09-04 15:33:21 -0700

    possible fix

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: 'index.html',
+        template: './index.html',
         // Output HTML file name
         title: 'J.A.T.E.', 
       }),
